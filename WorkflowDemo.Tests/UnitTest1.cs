46d372578f060732c6947390b5def79c33df018d
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-28 15:59:08 +0100

    demo van delay in workflowinvoker

--- WorkflowDemo.Tests/UnitTest1.cs
+++ WorkflowDemo.Tests/UnitTest1.cs
@@ -3,6 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
+using System.Activities.Statements;
+using Microsoft.VisualBasic.Activities;
+using Microsoft.CSharp.Activities;
+using System.Diagnostics;
 
 namespace WorkflowDemo.Tests
 {
@@ -21,6 +25,19 @@ namespace WorkflowDemo.Tests
             var result = invoker.Invoke(input);
 
             Assert.AreEqual("Goedemorgen workflow", result["Output"]);
+        }
+
+        [TestMethod]
+        public void WerktDeDelayActivityInEenWorkflowInvoker()
+        {
+            var sw = Stopwatch.StartNew();
+            var invoker = new WorkflowInvoker(new Delay
+            {
+                    Duration = new InArgument<TimeSpan>(TimeSpan.FromSeconds(5))
+            });
+            invoker.Invoke();
+
+            Assert.IsTrue(sw.Elapsed.TotalSeconds >= 5);
         }
     }
 }
