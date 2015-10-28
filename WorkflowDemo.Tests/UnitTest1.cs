defb5dc71f337f81b7b66a1dd0b7cf0a5fb2f6c0
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-28 11:14:40 +0100

    Unit test met workflowinvoker

---
+++ WorkflowDemo.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkflowDemo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WorkflowInvoker invoker = new WorkflowInvoker(new CodeActivity1());
+            var input = new Dictionary<string, object>
+            {
+                { "Text", "Goedemorgen" }
+            };
+
+            var result = invoker.Invoke(input);
+
+            Assert.AreEqual("Goedemorgen workflow", result["Output"]);
+        }
+    }
+}
---
+++ WorkflowDemo/CodeActivity1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using System.Security.Permissions;
+
+namespace WorkflowDemo
+{
+
+    public sealed class CodeActivity1 : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> Text { get; set; }
+
+        public OutArgument<string> Output { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            string text = context.GetValue(Text);
+
+            context.SetValue(Output, $"{text} workflow");
+        }
+    }
+}
